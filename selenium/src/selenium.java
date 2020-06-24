bff0128ebcb3534fc9aa5dcbd53d10342c1523c7
dktaak <dktaak23@gmail.com>
2020-06-24 20:21:18 +0530

    input from keyboard done

--- selenium/src/selenium.java
+++ selenium/src/selenium.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+import java.util.Scanner;//Scanner is a class is used to input from keyboard
 
 public class selenium {
 
@@ -21,10 +21,17 @@ public class selenium {
 		System.out.println("ur value is :"+ value);
 		char a = scan.next().charAt(2);//here next() is the method nd charAt(2) is the option nd value of parameter is showing index 2nd means 
 		                               //it will consider 3rd letter from a word
-		System.out.println("ur a is :"+ a);*/
-		
-		
-		
+		System.out.println("ur a is :"+ a);
+		for (int i=1;i<=3;i++) 
+		{
+			System.out.println("enter ur 10 digit ph no.");
+		long ph = scan.nextLong();
+		System.out.println("ur no. is :"+ ph);
+		}*/
+		int a=10,b=20;
+		System.out.println("ur no. is :"+(a+b));
+		System.out.println("a is ="+a+"\nb is ="+b);
+		System.out.println(a+b);
 	}
 
 }
