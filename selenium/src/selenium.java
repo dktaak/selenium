a9ce6af8f647c04a7be7dd7b5a1646f37c954418
dktaak <dktaak23@gmail.com>
2020-06-24 19:24:54 +0530

    inpout

---
+++ selenium/src/selenium.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class selenium {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("enter ur name");
+		String name = scan.nextLine();
+		System.out.println("ur name is :"+ name);
+		String city =scan.next();
+		System.out.println("ur city is :"+ city);
+		
+		
+
+	}
+
+}
