c33cda66b630b560cd9a51c05ba9f12614e6fa5d
dktaak <dktaak23@gmail.com>
2020-06-24 19:32:48 +0530

    long

--- selenium/src/selenium.java
+++ selenium/src/selenium.java
@@ -4,12 +4,14 @@ public class selenium {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		System.out.println("enter ur name");
+		/*System.out.println("enter ur name");
 		String name = scan.nextLine();
 		System.out.println("ur name is :"+ name);
 		String city =scan.next();
-		System.out.println("ur city is :"+ city);
-		
+		System.out.println("ur city is :"+ city);*/
+		System.out.println("enter ur no. is :");
+		long ph = scan.nextLong();
+		System.out.println("ur no. is :"+ ph);
 		
 
 	}
