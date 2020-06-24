79c5482dc24fd554df1dc7fd4925a3659659efec
dktaak <dktaak23@gmail.com>
2020-06-24 19:49:40 +0530

    char

--- selenium/src/selenium.java
+++ selenium/src/selenium.java
@@ -3,17 +3,28 @@ import java.util.Scanner;
 public class selenium {
 
 	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		/*System.out.println("enter ur name");
-		String name = scan.nextLine();
-		System.out.println("ur name is :"+ name);
+		Scanner scan = new Scanner(System.in);//Scanner is predefined class under java.util.Scanner
+		/*System.out.println("enter ur name");//scan is an obj of Scanner
+		String name = scan.nextLine();//nextLine() is a method to input complete data data
+		System.out.println("ur name is :"+ name);//next() inputs word only from a line
 		String city =scan.next();
-		System.out.println("ur city is :"+ city);*/
+		System.out.println("ur city is :"+ city);
 		System.out.println("enter ur no. is :");
 		long ph = scan.nextLong();
 		System.out.println("ur no. is :"+ ph);
+		int id = scan.nextInt();
+		System.out.println("ur id is :"+ id);
 		
-
+		double marks = scan.nextDouble();
+		System.out.println("ur marks is :"+ marks);
+		boolean value = scan.nextBoolean();
+		System.out.println("ur value is :"+ value);
+		char a = scan.next().charAt(2);//here next() is the method nd charAt(2) is the option nd value of parameter is showing index 2nd means 
+		                               //it will consider 3rd letter from a word
+		System.out.println("ur a is :"+ a);*/
+		
+		
+		
 	}
 
 }
