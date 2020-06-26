5fd8d674276f696b9f4382d86f54c6321ff33f16
dktaak <dktaak23@gmail.com>
2020-06-26 14:02:11 +0530

    exception

--- selenium/src/selenium.java
+++ selenium/src/selenium.java
@@ -102,7 +102,15 @@ public class selenium {
 		wrr.close();
 	    */
 		
+		int a=5,b=0,c=9;
+		 try 
+		 {  c=a/b;
+		 }
+		 catch(ArithmeticException e1)
+		 {
+			 System.out.println("divide by zero error"); }
 		
+		 System.out.println("no error");
 		
 		
 		
